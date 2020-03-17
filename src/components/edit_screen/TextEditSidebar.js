f162b0ff575a56ad8b8838cfad5b47accda2fcc5
wongRegina <wong.regina.1@gmail.com>
2020-03-17 17:56:05 -0400

    The edit sidebar has everything that is needed. Doesn't work yet

--- src/components/edit_screen/TextEditSidebar.js
+++ src/components/edit_screen/TextEditSidebar.js
@@ -22,13 +22,17 @@ class TextEditSidebar extends Component {
         this.props.doCallback();
     }
 
+    handleChangeText = () =>{
+        console.log("handleChangeText: ")
+    }
+
     handleTextColorChange = (event) => {
         console.log("handleTextColorChange to " + event.target.value);
         this.setState({ textColor: event.target.value }, this.completeUserEditing);
     }
 
     handleFontSizeChange = (event) => {
-        console.log("handleTextColorChangeComplete to " + event.target.value);
+        console.log("handleTextFontChangeComplete to " + event.target.value);
         this.setState({ fontSize: event.target.value }, this.completeUserEditing);
     }
 
@@ -51,16 +55,16 @@ class TextEditSidebar extends Component {
             <div className="card-panel col s4">
                 <div className="card blue-grey darken-1">
                     <div className="card-content white-text">
-                        <button className="waves-effect waves-light btn-small">&#9998;</button>
+                        <button className="waves-effect waves-light btn-small" onClick = {this.changeText}>&#9998;</button>
                         <button className={undoClass} onClick={this.handleUndo}>Undo</button>
                         <button className={doClass} onClick={this.handleDo}>Redo</button>
                     </div>
                 </div>
                 <div className="card blue-grey darken-1">
                     <div className="card-content white-text">
-                        <span className="card-title">Text</span>
+                        {/* <span className="card-title">Text</span> */}
                         <div className="row">
-                            <div className="col s4">Color:</div>
+                            <div className="col s4"> Text Color:</div>
                             <div className="col s8">
                                 <input type="color"
                                         onChange={this.handleTextColorChange}
@@ -70,6 +74,62 @@ class TextEditSidebar extends Component {
                         </div>
                         <div className="row">
                             <div className="col s4">Font Size:</div>
+                            <div className="col s8">
+                                <input type="range" min="4" max="144" 
+                                    onChange={this.handleFontSizeChange}
+                                    value={this.props.logo.fontSize} />
+                            </div>
+                        </div>
+                        {/* <span className="card-title">Background</span> */}
+                        <div className="row">
+                            <div className="col s4">Background Color:</div>
+                            <div className="col s8">
+                                <input type="color"
+                                        // TODO : Fix Handle
+                                        onChange={this.handleTextColorChange}
+                                        value={this.props.logo.textColor}
+                                />
+                            </div>
+                        </div>
+                        {/* <span className="card-title">Border</span> */}
+                        <div className="row">
+                            <div className="col s4">Border Color:</div>
+                            <div className="col s8">
+                                <input type="color"
+                                        // TODO : Fix Handle
+                                        onChange={this.handleTextColorChange}
+                                        value={this.props.logo.textColor}
+                                />
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col s4"> Border Radius:</div>
+                            <div className="col s8">
+                                <input type="range" min="4" max="144" 
+                                    onChange={this.handleFontSizeChange}
+                                    value={this.props.logo.fontSize} />
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col s4">Border Thickness:</div>
+                            <div className="col s8">
+                                <input type="range" min="4" max="144" 
+                                    onChange={this.handleFontSizeChange}
+                                    value={this.props.logo.fontSize} />
+                            </div>
+                        </div>
+                        {/* <span className="card-title">Padding</span> */}
+                        <div className="row">
+                            <div className="col s4">Padding:</div>
+                            <div className="col s8">
+                                <input type="range" min="4" max="144" 
+                                    onChange={this.handleFontSizeChange}
+                                    value={this.props.logo.fontSize} />
+                            </div>
+                        </div>
+                        {/* <span className="card-title">Margin</span> */}
+                        <div className="row">
+                            <div className="col s4">Margin:</div>
                             <div className="col s8">
                                 <input type="range" min="4" max="144" 
                                     onChange={this.handleFontSizeChange}
